cb16ec0123de8eb827147b23cde0fee458122bc8
kanwaladreeskhan <khanadreiskhan@gmail.com>
2025-12-12 09:58:39 +0500

    Commit to main

---
+++ MinHeapSort.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeapSort
+{
+    internal class MinHeapSort
+    {
+        public void Sort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = n / 2 - 1; i >= 0; i--)
+            {
+                Heapify(arr, n, i);
+            }
+            
+                for(int i = 0; i < n; i++)
+                {
+                    int min = arr[0];
+                    arr[0] = arr[n-1-i];
+                    arr[n-1-i] = min;
+                    Heapify(arr, n - 1 - i, 0);
+                   
+                }
+            Console.WriteLine("Descending Sorting.....");
+            Print(arr);
+        }
+        private void Heapify(int[] arr, int n, int i)
+        {
+            int smallest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            if (left < n && arr[left] < arr[smallest])
+            {
+                smallest = left;
+            }
+            if (right < n && arr[right] < arr[smallest])
+            {
+                smallest = right;
+            }
+            if (smallest != i)
+            {
+                int temp = arr[i];
+                arr[i] = arr[smallest];
+                arr[smallest] = temp;
+                Heapify(arr, n, smallest);
+            }
+        }
+        public void Print(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + "  ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeapSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 9, 4, 7, 1, 3, 6, 2 };
+            MinHeapSort heap = new MinHeapSort();
+            heap.Sort(arr);
+            
+
+        }
+    }
+}
